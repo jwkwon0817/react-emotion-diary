a42ef24505087952812bc7e3008e9ff43083afa5
jwkwon0817 <=>
2023-06-05 00:16:39 +0900

    MyHeader JSX

---
+++ src/components/MyHeader.jsx
@@ -0,0 +1,9 @@
+export const MyHeader = ({ headText, leftChild, rightChild }) => {
+	return (
+		<header>
+			<div className={ 'head-btn-left' }>{ leftChild }</div>
+			<div className={ 'head-text' }>{ headText }</div>
+			<div className={ 'head-btn-right' }>{ rightChild }</div>
+		</header>
+	);
+};
