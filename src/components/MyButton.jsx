0b12c306ced1fab44be91746b773fce286bb153c
jwkwon0817 <=>
2023-06-05 00:16:34 +0900

    MyButton JSX

---
+++ src/components/MyButton.jsx
@@ -0,0 +1,11 @@
+export const MyButton = ({ text, type, onClick }) => {
+	const btnType = [ 'positive', 'negative' ].includes(type) ? type : 'default';
+	
+	return (
+		<button className={ [ 'MyButton', `MyButton-${ btnType }` ].join(' ') } onClick={ onClick }>{ text }</button>
+	);
+};
+
+MyButton.defaultProps = {
+	type: 'default',
+};
